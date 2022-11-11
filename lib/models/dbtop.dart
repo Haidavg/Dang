e2a59d7d23452be9156d297683260b97f0a4e317
quocthai027 <113010699+quocthai027@users.noreply.github.com>
2022-11-11 11:13:38 +0700

    ThaiaddModelsTop

---
+++ lib/models/dbtop.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_application_2/main_xep_hang.dart';
+
+class name{
+  name(
+    {
+      required this.hinh,
+      required this.ten,
+      required this.top,
+    }
+
+  );
+  String hinh;
+  String ten;
+  String top;
+}
+List <name> list_top_nguoi_choi=[
+  name(hinh:   "https://6.viki.io/image/9c15adec53ea43dfa3e2ce0c77c83c1f.jpeg?s=900x600&e=t",
+   ten: "Nguyễn Anh Kha",
+   top:"https://png.pngtree.com/png-clipart/20191120/original/pngtree-ranking-1-illustration-1-gold-vector-flat-illustration-ranked-1-for-png-image_5063286.jpg"),
+   name(hinh:   "https://6.viki.io/image/9c15adec53ea43dfa3e2ce0c77c83c1f.jpeg?s=900x600&e=t",
+   ten: "Nguyễn Như Huỳnh",
+   top:"https://png.pngtree.com/png-clipart/20191120/original/pngtree-ranking-2-silver-illustration-png-image_5063261.jpg"),
+   name(hinh:   "https://6.viki.io/image/9c15adec53ea43dfa3e2ce0c77c83c1f.jpeg?s=900x600&e=t",
+   ten: "Nguyễn Như Huỳnh",
+   top:"https://png.pngtree.com/png-clipart/20191120/original/pngtree-ranking-3-bronze-illustration-png-image_5063260.jpg"),
+   
+   
+];
