d3fb7587fc9ec4912b08ba8481de87e6c4a28473
quocthai027 <113010699+quocthai027@users.noreply.github.com>
2022-11-11 11:12:52 +0700

    ThaiaddModelsBanBe

---
+++ lib/models/dbbanbe.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_application_2/main_ban_be.dart';
+
+class name{
+  name(
+    {
+      required this.hinh,
+      required this.ten,
+    }
+
+  );
+  String hinh;
+  String ten;
+}
+List <name> list_ban_be=[
+  name(hinh:   "https://6.viki.io/image/9c15adec53ea43dfa3e2ce0c77c83c1f.jpeg?s=900x600&e=t",
+   ten: "Nguyễn Anh Kha"),
+   name(hinh:   "https://6.viki.io/image/9c15adec53ea43dfa3e2ce0c77c83c1f.jpeg?s=900x600&e=t",
+   ten: "Nguyễn Như Huỳnh"),
+   
+];
