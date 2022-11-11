337806b66b24b4a669d5a11edfaef7b7e091d99d
quocthai027 <113010699+quocthai027@users.noreply.github.com>
2022-11-11 11:11:07 +0700

    ThaiaddCompomentBanBe

---
+++ lib/compoment/banbe.dart
@@ -0,0 +1,74 @@
+import '../models/dbbanbe.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+
+class BanBe extends StatefulWidget {
+  const BanBe({super.key, required this.a});
+  final name a;
+
+  @override
+  State<BanBe> createState() => _BanBe();
+}
+
+class _BanBe extends State<BanBe> {
+  @override
+  Widget build(BuildContext context) {
+    return
+    Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 153, 217, 234),
+      ),
+      child:
+      Container(
+        
+        margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(width: 2, color: Colors.black),
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.blueAccent.withOpacity(0.9),
+            blurRadius: 8,
+            // offset: const Offset(0.0, 0.0),
+          )
+        ],
+      ),
+      child: Row(
+        children: [
+           Container(
+            width: 120,
+            child: Expanded(child: Image(image:NetworkImage(widget.a.hinh)))),
+          Expanded(child: Text(widget.a.ten,style: TextStyle(fontSize: 17),)),
+         Container(
+          width: 60,
+           child: Expanded(child:  
+                     TextButton.icon(
+                 style: TextButton.styleFrom(
+
+                   primary: Colors.black,
+                    side: BorderSide(color: Colors.black),
+                    shadowColor: Colors.blue.withOpacity(0.5),
+                    backgroundColor: Colors.grey,
+                   shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(30),
+                 ),
+                    // text + icon color
+                 ),
+                 icon: Icon(Icons.delete, size: 25),
+                 label: Text('',),
+                 onPressed: () {},
+               ),  
+
+           ),
+         ),
+        ],
+      ),
+      )
+    ); 
+  
+  }
+}
