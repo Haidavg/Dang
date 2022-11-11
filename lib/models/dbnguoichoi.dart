d5a69a214de72d69303348f3949871c28542c600
quocthai027 <113010699+quocthai027@users.noreply.github.com>
2022-11-11 11:13:20 +0700

    ThaiaddModelsNguoiChoi

---
+++ lib/models/dbnguoichoi.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_application_2/main_nguoi_choi.dart';
+
+class name{
+  name(
+    {
+      required this.hinh,
+      required this.ten,
+    }
+
+  );
+  String hinh;
+  String ten;
+}
+List <name> list_nguoi_choi=[
+  name(hinh:   "https://6.viki.io/image/9c15adec53ea43dfa3e2ce0c77c83c1f.jpeg?s=900x600&e=t",
+   ten: "Nguyễn Anh Kha"),
+   name(hinh:   "https://6.viki.io/image/9c15adec53ea43dfa3e2ce0c77c83c1f.jpeg?s=900x600&e=t",
+   ten: "Nguyễn Như Huỳnh"),
+   name(hinh:  "https://6.viki.io/image/9c15adec53ea43dfa3e2ce0c77c83c1f.jpeg?s=900x600&e=t",
+   ten: "Cao Tấn Phát"),
+   name(hinh:  "https://6.viki.io/image/9c15adec53ea43dfa3e2ce0c77c83c1f.jpeg?s=900x600&e=t",
+   ten: " Trần Anh Minh"),
+   name(hinh: "https://6.viki.io/image/9c15adec53ea43dfa3e2ce0c77c83c1f.jpeg?s=900x600&e=t",
+   ten: "Cao Bá Nhật"),
+   name(hinh:   "https://6.viki.io/image/9c15adec53ea43dfa3e2ce0c77c83c1f.jpeg?s=900x600&e=t",
+   ten: "Nguyễn Thị Thùy Linh"),
+];
